c5cdeff1f21b7e27c92c35a31ee375670dc43079
santiagopardal <santiagopardal@outlook.com>
2020-12-07 16:56:37 -0300

    Training

--- CNNs.py
+++ CNNs.py
@@ -32,36 +32,3 @@ def create_model():
     model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
 
     return model
-
-
-def create_3d_model():
-    model = tf.keras.models.Sequential([
-
-        tf.keras.layers.Conv3D(64, kernel_size=(5, 5), activation="relu", input_shape=(256, 144, 150)),
-        tf.keras.layers.MaxPooling3D(pool_size=3, strides=2),
-        tf.keras.layers.BatchNormalization(),
-
-        tf.keras.layers.Conv3D(64, kernel_size=(5, 5), activation="relu"),
-        tf.keras.layers.MaxPooling3D(pool_size=3, strides=2),
-        tf.keras.layers.BatchNormalization(),
-
-        tf.keras.layers.Conv3D(128, kernel_size=(5, 5), activation="relu"),
-        tf.keras.layers.MaxPooling3D(pool_size=3, strides=2),
-        tf.keras.layers.BatchNormalization(),
-
-        tf.keras.layers.Conv3D(256, kernel_size=(5, 5), activation="relu"),
-        tf.keras.layers.MaxPooling3D(pool_size=2, strides=2),
-
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dropout(0.5),
-
-        tf.keras.layers.Dense(512, activation="relu"),
-        tf.keras.layers.Dense(1, activation="sigmoid")
-    ])
-
-    optimizer = tf.keras.optimizers.Adam(lr=1e-2)
-    loss = tf.keras.losses.BinaryCrossentropy()
-
-    model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
-
-    return model
--- Camera.py
+++ Camera.py
@@ -10,7 +10,7 @@ from PIL import Image
 from MotionEventHandler import MotionEventHandler
 from Frame import Frame
 from Observers import Observer
-from Observers import TrainerObserver#Observer
+from Observers import Observer
 from threading import Semaphore
 
 
@@ -23,7 +23,7 @@ class Camera:
         self._record_thread = None
         self._kill_thread = False
         self._motion_handler = MotionEventHandler()
-        self._observer = TrainerObserver(self)#Observer(self)
+        self._observer = Observer(self)
         self._observe_semaphore = Semaphore(0)
         self._frames_to_observe = []
 
