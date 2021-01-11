2fc2bd96817f726c47de8025baaeefa575949219
santiagopardal <santiagopardal@outlook.com>
2021-01-10 22:10:11 -0300

    Added statistics

--- System.py
+++ System.py
@@ -7,6 +7,7 @@ import Constants
 import datetime
 from GUI.KivyGUI import CamerAI
 from threading import Thread, Semaphore
+import plotly.express as px
 
 
 class System:
@@ -87,3 +88,70 @@ class System:
 
     def __upload(self, path: str):
         pass
+
+    @staticmethod
+    def create_statistics():
+        for place in os.listdir(Constants.STORING_PATH):
+            place_path = os.path.join(Constants.STORING_PATH, place)
+
+            if ".DS" not in place and os.path.isdir(place_path):
+                dates_in_place = []
+
+                if os.path.exists(os.path.join(place_path, "dates.pck")):
+                    with open(os.path.join(place_path, "dates.pck"), "rb") as handle:
+                        dates_in_place = pickle.load(handle)
+
+                for day in os.listdir(place_path):
+                    day_path = os.path.join(place_path, day)
+                    if ".DS" not in day and os.path.isdir(day_path):
+
+                        year = int(day[:4])
+                        month = int(day[:7][5:])
+                        dei = int(day[8:])
+
+                        dates = []
+
+                        for file in os.listdir(day_path):
+                            if ".DS" not in day:
+                                file = file[:len(file)-5]
+                                hour = int(file[:2])
+                                minute = int(file[:5][3:])
+                                seconds = int(float(file[6:]))
+                                date = datetime.datetime(year=year, month=month, day=dei,
+                                                         hour=hour, minute=minute, second=seconds)
+                                dates.append(date)
+
+                        dates_in_place.append(dates)
+
+                with open(os.path.join(place_path, "dates.pck"), "wb") as handle:
+                    pickle.dump(dates_in_place, handle)
+
+            print(dates_in_place)
+
+    @staticmethod
+    def display_statistics():
+        for place in os.listdir(Constants.STORING_PATH):
+            place_path = os.path.join(Constants.STORING_PATH, place)
+
+            dates = []
+            print(os.path.join(place_path, "dates.pck"))
+            if os.path.exists(os.path.join(place_path, "dates.pck")):
+                with open(os.path.join(place_path, "dates.pck"), "rb") as handle:
+                    dates = pickle.load(handle)
+
+            data = {}
+
+            for lista in dates:
+                for date in lista:
+                    date: datetime.datetime
+                    start_of_day = datetime.datetime(date.year, date.month, date.day)
+
+                    seconds = (date - start_of_day).total_seconds()
+
+                    if seconds in data:
+                        data[seconds] += 1
+                    else:
+                        data[seconds] = 1
+
+            fig = px.scatter(x=[int(val) for val in data.keys()], y=list(data.values()))
+            fig.show()
