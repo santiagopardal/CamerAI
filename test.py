445e314e15f5c2f09ba4ef2c439bc9b900a1152d
santiagopardal <santiagopardal@outlook.com>
2021-01-13 14:21:26 -0300

    Cleaned test

--- test.py
+++ test.py
@@ -1,21 +1,5 @@
 from System import System
-from Cameras.Camera import FI9803PV3, FI89182
-import Constants
-from Handlers.Handler import FrameHandler
 
 
 sys = System()
-
-cameras = [
-    FI9803PV3("192.168.1.131", 1111, 1113, "Front Yard", Constants.USER, Constants.PASSWORD),
-    FI89182("192.168.1.133", 2222, "Front Yard 2", Constants.USER, Constants.PASSWORD),
-    FI9803PV3("192.168.1.132", 4444, 4446, "Back Yard", Constants.USER, Constants.PASSWORD),
-    FI9803PV3("192.168.1.130", 3333, 3335, "Back Yard 2", Constants.USER, Constants.PASSWORD)
-]
-
-for camera in cameras:
-    fh = FrameHandler(camera)
-    camera.set_frames_handler(fh)
-
-sys.add_cameras(cameras)
 sys.run_with_gui()
