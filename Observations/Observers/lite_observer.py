065e32c692e5a02abb499ffe159b3f6e14d1dd2b
Santiago Pardal <santiagopardal@outlook.com>
2021-08-23 13:13:22 -0300

    Revert "Revert "testing singleton interpreter""
    This reverts commit ef328b4d10316e0497ae9ccbe8d317cd39674c98.

--- Observations/Observers/lite_observer.py
+++ Observations/Observers/lite_observer.py
@@ -1,13 +1,43 @@
 from Observations.Observers.observer import Observer
 from Observations.Models.TFLiteMovementDetector import TFLiteModelDetector
 import numpy as np
+from threading import Semaphore
+from Cameras.frame import Frame
 
 
 class LiteObserver(Observer):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+
+        return cls._instance
+
     def __init__(self):
         super().__init__(TFLiteModelDetector())
+
+        self._mutex = Semaphore(1)
 
     def _prepare_for_cnn(self, previous_frame, frame):
         res = super()._prepare_for_cnn(previous_frame, frame)
 
         return np.expand_dims(res, axis=0)
+
+    def _batch_movement_check(self, frames: list) -> list:
+        self._mutex.acquire()
+
+        res = super()._batch_movement_check(frames)
+
+        self._mutex.release()
+
+        return res
+
+    def _movement(self, previous_frame: Frame, frame: Frame) -> bool:
+        self._mutex.acquire()
+
+        res = super()._movement(previous_frame, frame)
+
+        self._mutex.release()
+
+        return res
