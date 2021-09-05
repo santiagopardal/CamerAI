91cc112e4dc970b845260b1105687f62a1645c7d
santiagopardal <santiagopardal@outlook.com>
2021-09-05 16:58:24 -0300

    reducing multithreading

--- src/Handlers/asynchronous_disk_store_motion_handler.py
+++ src/Handlers/asynchronous_disk_store_motion_handler.py
@@ -1,6 +1,5 @@
 from src.Handlers.motion_handler import MotionHandler
 from collections import deque
-from threading import Thread, Semaphore
 import os
 from src.constants import STORING_PATH
 import cv2
@@ -23,7 +22,6 @@ class AsynchronousDiskStoreMotionHandler(MotionHandler):
         if seconds_to_buffer:
             self._frames.append([])
 
-        self._frames_ready = Semaphore(0)
         self._done = False
         self._storing_path = os.path.join(STORING_PATH, storing_path)
 
@@ -32,9 +30,6 @@ class AsynchronousDiskStoreMotionHandler(MotionHandler):
 
         self._buffer_size = seconds_to_buffer*frame_rate
 
-        self._background_thread = Thread(target=self._store, args=())
-        self._background_thread.start()
-
         super().__init__()
 
     def handle(self, event: list):
@@ -42,59 +37,24 @@ class AsynchronousDiskStoreMotionHandler(MotionHandler):
         Receives the frames and once the handler is ready stores them.
         :param event: List of frames in which there has been movement.
         """
-        if event and not self._done:
+        if event:
             if self._buffer_size:
                 self._frames[0] = self._frames[0] + event
 
                 if len(self._frames[0]) >= self._buffer_size:
+                    to_store = self._frames.popleft()
                     self._frames.append([])
-                    self._frames_ready.release()
+                    self._test_store(to_store)
             else:
-                self._frames.append(event)
-                self._frames_ready.release()
+                self._test_store([event])
 
-    def stop(self):
-        """
-        Stops the background thread running _store.
-        """
-        self._done = True
-        self._frames_ready.release()
-        self._background_thread.join()
-        self._done = False
+    def _test_store(self, frames):
+        if self._buffer_size:
+            filename = "{}.mp4".format(frames[0].time)
 
-        while self._frames_ready.acquire(blocking=False):
-            continue
-
-    def _store(self):
-        """
-        Waits for frames to be ready and stores them on disk.
-        """
-        while not self._done:
-            self._frames_ready.acquire()
-
-            if self._frames:
-                frames = self._frames.popleft()
-
-                if self._buffer_size:
-                    filename = "{}.mp4".format(frames[0].time)
-
-                    self._store_video(frames, filename)
-                else:
-                    frames[0].store(self._storing_path)
-
-                del frames
-
-        while self._frames:
-            frames = self._frames.popleft()
-
-            if self._buffer_size:
-                filename = "{}.mp4".format(frames[0].time)
-
-                self._store_video(frames, filename)
-            else:
-                frames[0].store(self._storing_path)
-
-            del frames
+            self._store_video(frames, filename)
+        else:
+            frames[0].store(self._storing_path)
 
     def _store_video(self, frames, filename):
         month = frames[0].date.month if frames[0].date.month > 9 else "0{}".format(frames[0].date.month)
@@ -115,7 +75,7 @@ class AsynchronousDiskStoreMotionHandler(MotionHandler):
         for frame in frames:
             try:
                 video.write(frame.frame)
-            except:
+            except Exception:
                 pass
 
         video.release()
