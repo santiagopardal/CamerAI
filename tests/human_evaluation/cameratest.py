71fb6b35665ab2c441c5679a361d9a0691a96062
Santiago Pardal <santiagopardal@outlook.com>
2022-02-13 12:09:21 -0300

    clean code

--- tests/human_evaluation/cameratest.py
+++ tests/human_evaluation/cameratest.py
@@ -1,19 +1,22 @@
 import cv2
 import numpy as np
-import threading
 import urllib
+import concurrent.futures
 from src.observations.models.v3_model_motion_detector import V3MotionDetector
+from src.constants import CNN_INPUT_SHAPE
 
 
-detector = V3MotionDetector()
+MODEL = V3MotionDetector()
+POOL = concurrent.futures.ThreadPoolExecutor(5)
+CAMERA_URL = "rtsp://{}:{}@192.168.0.131:554/videoMain".format(urllib.parse.quote("admin"), urllib.parse.quote("*{-4s#aG*_>2"))
 
 
 def resize(frame):
-    return cv2.resize(frame, (180, 180), interpolation=cv2.INTER_AREA)
+    return cv2.resize(frame, (CNN_INPUT_SHAPE[0], CNN_INPUT_SHAPE[1]), interpolation=cv2.INTER_AREA)
 
 
 def resize_and_grayscale(frame):
-    img = cv2.resize(frame, (180, 180), interpolation=cv2.INTER_AREA)
+    img = resize(frame=frame)
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 
@@ -27,9 +30,9 @@ def get_diff(previous_frame, frame):
 def detect_movement(diff):
     diff = np.array(diff / 255, dtype="float32")
 
-    images = np.array([diff]).reshape((180, 180, 1))
+    images = np.array([diff]).reshape(CNN_INPUT_SHAPE)
 
-    movement = detector.predict(images)
+    movement = MODEL.predict(images)
 
     if movement >= 0.6:
         print(movement)
@@ -42,33 +45,29 @@ def diff_without_resize(previous, actual):
 
 
 def show_video():
-    url = "rtsp://{}:{}@192.168.0.131:554/videoMain".format(urllib.parse.quote("admin"), urllib.parse.quote("*{-4s#aG*_>2"))
-    cap = cv2.VideoCapture(url)
+    cap = cv2.VideoCapture(CAMERA_URL)
 
     _, previous_image = cap.read()
+    close = False
 
-    while cap.isOpened():
-        try:
-            ret, frame = cap.read()
+    while cap.isOpened() and not close:
+        ret, frame = cap.read()
 
-            cv2.imshow('Video', frame)
-            diff = diff_without_resize(previous_image, frame)
-            cv2.imshow('Diff', diff)
-            diff = resize(diff)
-            cv2.imshow('CNN', diff)
+        cv2.imshow('Video', frame)
+        diff = diff_without_resize(previous_image, frame)
+        cv2.imshow('Diff', diff)
+        diff = resize(diff)
+        cv2.imshow('CNN', diff)
 
-            thread = threading.Thread(target=detect_movement, args=(diff,))
-            thread.daemon = True
-            thread.start()
+        POOL.submit(detect_movement, diff)
+        previous_image = frame
 
-            previous_image = frame
-
-            if cv2.waitKey(20) & 0xFF == ord('q'):
-                break
-        except Exception as e:
-            print(e)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            close = True
 
     cap.release()
     cv2.destroyAllWindows()
 
-show_video()
+
+if __name__ == '__main__':
+    show_video()
