76623fbad18a5a5a81c224ff2f04489c5ac56a7e
Santiago Pardal <santiagopardal@outlook.com>
2022-08-06 15:31:56 -0300

    add cron job instead of scheduling myself, also add new endpoint for temporal videos

---
+++ src/merger/merge.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+import src.api.temporal_videos as temporal_videos_api
+from src.utils.date_utils import get_numbers_as_string
+import src.api.videos as videos_api
+from src.api.cameras import get_cameras
+import os
+from src.constants import STORING_PATH
+from libs.VideosMerger.videos_iterator import VideosIterator
+from libs.VideosMerger.merger import VideoMerger
+import src.media.video.video_factory as video_factory
+
+
+def merge_cameras_video(camera: dict, date: datetime):
+    temporal_videos = temporal_videos_api.get_temporal_videos(camera['id'], date)
+
+    if temporal_videos:
+        pth = os.path.join(STORING_PATH, camera['name'])
+
+        day, month, year = get_numbers_as_string(date)
+        video_path = "{}/{}-{}-{}.mp4".format(pth, year, month, day)
+
+        videos = VideosIterator(temporal_videos, video_factory)
+        merger = VideoMerger(videos)
+        merger.merge(video_path, True)
+
+        videos_api.register_new_video(camera['id'], date, video_path)
+
+
+def transform_yesterday_into_video():
+    yesterday = datetime.now() - timedelta(days=1)
+    cameras = get_cameras()
+
+    for camera in cameras:
+        try:
+            merge_cameras_video(camera, yesterday)
+        except Exception as e:
+            print("Error merging videos:", e)
+
+
+if __name__ == '__main__':
+    transform_yesterday_into_video()
--- src/node.py
+++ src/node.py
@@ -1,17 +1,9 @@
 import os
-from threading import Thread
 from src.cameras.serializer import deserialize
 import src.constants as constants
-from src.utils.date_utils import get_numbers_as_string
-from datetime import timedelta, datetime
-import sched
 import time
-import src.api.videos as videos_api
+from threading import Semaphore
 import src.api.cameras as cameras_api
-import src.api.temporal_videos as temporal_videos_api
-from libs.VideosMerger.merger import VideoMerger
-from libs.VideosMerger.videos_iterator import VideosIterator
-import src.media.video.video_factory as video_factory
 
 
 class Node:
@@ -22,15 +14,13 @@ class Node:
             os.mkdir(constants.STORING_PATH)
 
         self._fetch_cameras_from_api()
-
-        self.__scheduler = sched.scheduler(time.time, time.sleep)
-        self.__schedule_video_transformation()
+        self.waiter = Semaphore(0)
 
     def run(self):
         for camera in self.cameras:
             camera.receive_video()
 
-        self.__scheduler.run()
+        self.waiter.acquire()
 
         for camera in self.cameras:
             camera.stop_receiving_video()
@@ -43,16 +33,8 @@ class Node:
         for camera in self.cameras:
             camera.stop_recording()
 
-    def __schedule_video_transformation(self):
-        now = datetime.now()
-        tomorrow_3_am = now + timedelta(days=1) - timedelta(hours=now.hour) - timedelta(minutes=now.minute) - \
-                        timedelta(seconds=now.second) - timedelta(microseconds=now.microsecond) + timedelta(hours=3)
-
-        time_until_3 = tomorrow_3_am - now
-        time_until_3 = time_until_3.total_seconds()
-
-        self.__scheduler.enter(time_until_3, 1, self._transform_yesterday_into_video)
-        print("Scheduled video transformation in {} seconds!".format(time_until_3))
+    def stop(self):
+        self.waiter.release()
 
     def _fetch_cameras_from_api(self):
         i = 0
@@ -68,28 +50,3 @@ class Node:
                 seconds = 2 ** i
                 print("Could not fetch from API, retrying in {} seconds".format(seconds))
                 time.sleep(seconds)
-
-    def _transform_yesterday_into_video(self):
-        self.__schedule_video_transformation()
-
-        yesterday = datetime.now() - timedelta(days=1)
-
-        for camera in self.cameras:
-            try:
-                self._merge_cameras_video(camera, yesterday)
-            except Exception as e:
-                print("Error merging videos:", e)
-
-    def _merge_cameras_video(self, camera, date: datetime):
-        temporal_videos = temporal_videos_api.get_temporal_videos(camera.id, date)
-
-        pth = os.path.join(constants.STORING_PATH, camera.name)
-
-        day, month, year = get_numbers_as_string(date)
-        video_path = "{}/{}-{}-{}.mp4".format(pth, year, month, day)
-
-        videos = VideosIterator(temporal_videos, video_factory)
-        merger = VideoMerger(videos)
-        merger.merge(video_path, True)
-
-        videos_api.register_new_video(camera.id, date, video_path)
