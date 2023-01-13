5d9347cfdaea705f12e840b392b772ee4560066a
Santiago Pardal <santiagopardal@outlook.com>
2023-01-12 22:02:27 -0300

    modular

---
+++ src/cameras/__init__.py
@@ -0,0 +1,6 @@
+from .camera import Camera
+from .foscam import FI89182
+from .foscam import FI9803PV3
+from .retrieval_strategy import RetrievalStrategy
+from .retrieval_strategy import LiveRetrievalStrategy
+from .retrieval_strategy import ScreenshotRetrievalStrategy
