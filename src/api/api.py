48a83713d3f0027ef6a1e6ecb6dc04d6da625cfa
Santiago Pardal <santiagopardal@outlook.com>
2023-08-07 22:26:45 -0300

    cleaner code

--- src/api/api.py
+++ src/api/api.py
@@ -1,4 +1,4 @@
-from requests import Session
+from requests import Session, Response
 from src.constants import API_URL
 import time
 import logging
@@ -11,57 +11,49 @@ def set_headers(headers: dict):
     SESSION.headers = headers
 
 
-def get(endpoint: str):
-    api_endpoint = "{}/{}".format(API_URL, endpoint)
-
+def _execute_and_retry_on_failure(callback, error_message: str) -> Response:
     for i in range(6):
         try:
-            return SESSION.get(api_endpoint)
+            return callback()
         except Exception as e:
-            logging.info(f"Error getting @ {endpoint}: {str(e)}")
+            logging.info(f"{error_message}: {str(e)}")
             if i == 5:
                 raise e
 
             time.sleep(1)
 
 
-def post(endpoint: str, body: dict = None):
+def get(endpoint: str) -> Response:
     api_endpoint = "{}/{}".format(API_URL, endpoint)
 
-    for i in range(6):
-        try:
-            return SESSION.post(api_endpoint, data=body)
-        except Exception as e:
-            logging.info(f"Error posting @ {endpoint}: {str(e)}")
-            if i == 5:
-                raise e
+    def do_get() -> Response:
+        return SESSION.get(api_endpoint)
 
-            time.sleep(1)
+    return _execute_and_retry_on_failure(do_get, f"Error getting @ {endpoint}")
 
 
-def put(endpoint: str, body: dict = None):
+def post(endpoint: str, body: dict = None) -> Response:
     api_endpoint = "{}/{}".format(API_URL, endpoint)
 
-    for i in range(6):
-        try:
-            return SESSION.put(api_endpoint, data=body)
-        except Exception as e:
-            logging.info(f"Error putting @ {endpoint}: {str(e)}")
-            if i == 5:
-                raise e
+    def do_post() -> Response:
+        return SESSION.post(api_endpoint, data=body)
 
-            time.sleep(1)
+    return _execute_and_retry_on_failure(do_post, f"Error posting @ {endpoint}")
 
 
-def delete(endpoint: str):
+def put(endpoint: str, body: dict = None) -> Response:
     api_endpoint = "{}/{}".format(API_URL, endpoint)
 
-    for i in range(6):
-        try:
-            return SESSION.delete(api_endpoint)
-        except Exception as e:
-            logging.info(f"Error deleting @ {endpoint}: {str(e)}")
-            if i == 5:
-                raise e
+    def do_put() -> Response:
+        return SESSION.put(api_endpoint, data=body)
 
-            time.sleep(1)
+    return _execute_and_retry_on_failure(do_put, f"Error putting @ {endpoint}")
+
+
+def delete(endpoint: str) -> Response:
+    api_endpoint = "{}/{}".format(API_URL, endpoint)
+
+    def do_delete() -> Response:
+        return SESSION.delete(api_endpoint)
+
+    return _execute_and_retry_on_failure(do_delete, f"Error deleting @ {endpoint}")
