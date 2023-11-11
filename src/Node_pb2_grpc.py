980937aa5421dd645ef4c618ef0dcc9deed5174b
Santiago Pardal <santiagopardal@outlook.com>
2023-11-11 14:43:36 -0300

    many changes

--- src/Node_pb2_grpc.py
+++ src/Node_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import Node_pb2 as Node__pb2
+import src.Node_pb2 as Node__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from google.protobuf import wrappers_pb2 as google_dot_protobuf_dot_wrappers__pb2
 
