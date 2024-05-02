2913f792a8a280cbd41621aabd189fc6177a8228
Santiago Pardal <santiagopardal@outlook.com>
2024-05-01 21:36:11 -0300

    bugfix

--- src/cameras/retrieval_strategy/retrieval_strategy.py
+++ src/cameras/retrieval_strategy/retrieval_strategy.py
@@ -2,6 +2,7 @@ import logging
 from concurrent.futures import ThreadPoolExecutor
 from numpy import ndarray
 from src.cameras import Camera
+from src.handlers import FrameHandler
 
 
 class RetrievalStrategy:
@@ -9,17 +10,20 @@ class RetrievalStrategy:
     def __init__(self, camera: Camera):
         self._camera = camera
         self._should_receive_frames = False
-        self._thread_pool = ThreadPoolExecutor(max_workers=1)
+        self._thread_pool = None
+        self._frame_handler = FrameHandler()
 
     def connect(self):
         pass
 
     def receive_video(self):
         self._should_receive_frames = True
+        self._thread_pool = ThreadPoolExecutor(max_workers=1)
         self._thread_pool.submit(self._receive_frames)
 
     def stop_receiving_video(self):
         self._should_receive_frames = False
+        self._thread_pool.shutdown()
 
     def retrieve(self) -> ndarray:
         pass
