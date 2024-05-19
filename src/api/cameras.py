6366037e39406d9ab919b94a9d1f9496d6ef158a
Santiago Pardal <santiagopardal@outlook.com>
2024-05-19 02:30:59 -0300

    Code style

--- src/api/cameras.py
+++ src/api/cameras.py
@@ -6,5 +6,6 @@ def get_cameras(id: int) -> list:
     response = API.get(f"cameras/node/{id}")
     return response.json()
 
+
 def log_connection_status(camera_id: int, message: str, date: datetime):
     return API.post(f"cameras/{camera_id}/connection_status", {"message": message, "date": date})
